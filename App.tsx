06547089683112f0664e8d46a13598b87294c37f
AntLacerda <lacerdaneto99@hotmail.com>
2024-08-26 21:34:13 -0300

    Iniciando a primeira tela

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Login from './src/screens/Login';
 
 export default function App() {
   return (
---
+++ src/screens/Login/index.tsx
@@ -0,0 +1,8 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Login(){
+    return(
+        <Text>Teste</Text>
+    )
+}
+
