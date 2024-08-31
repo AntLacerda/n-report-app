cdbb95b0167fe5a306aad28b98c55480a3de2902
João Darwin <joaodarwin.ip22@gmail.com>
2024-08-31 09:11:17 -0300

    feat: Criar rotas publicas do aplicativo
    Issue: #18

---
+++ src/routes/public/index.tsx
@@ -0,0 +1,16 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Login from "../../screens/Login";
+import Signup from "../../screens/Signup";
+
+const { Navigator, Screen } = createNativeStackNavigator();
+
+const PublicStackNavigation = () => {
+    return (
+        <Navigator initialRouteName="login">
+            <Screen name="login" component={Login}/>
+            <Screen name="signup" component={Signup}/>
+        </Navigator>
+    )
+}
+
+export default PublicStackNavigation;
