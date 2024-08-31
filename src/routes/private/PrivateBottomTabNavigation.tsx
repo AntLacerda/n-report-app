4670192a2e1aa75695aa919043ae74ef88fb42f5
João Darwin <joaodarwin.ip22@gmail.com>
2024-08-31 11:06:35 -0300

    feat: Adicionar ícones a tabs
    Issue: #18

--- src/routes/private/PrivateBottomTabNavigation.tsx
+++ src/routes/private/PrivateBottomTabNavigation.tsx
@@ -1,4 +1,5 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from '@expo/vector-icons/Ionicons';
 import Home from "../../screens/Home";
 import Map from "../../screens/Map";
 import Report from "../../screens/Report";
@@ -16,12 +17,48 @@ const PrivateBottomTabNavigation = () => {
             tabBarStyle: {
                 backgroundColor: "#464646"
             },
-            // tabBarHideOnKeyboard: true,
+            tabBarHideOnKeyboard: true,
         })}>
-            <Screen name="Home" component={Home} />
-            <Screen name="Map" component={Map} />
-            <Screen name="Report" component={Report} />
-            <Screen name="Profile" component={Profile} />
+            <Screen name="Home" component={Home} options={{
+                tabBarLabel: "Início",
+                tabBarIcon: ({ focused, color, size }) => (
+                    <Ionicons
+                        name={focused ? "home-outline" : "home-sharp"}
+                        color={color}
+                        size={size}
+                    />
+                )
+            }} />
+            <Screen name="Map" component={Map} options={{
+                tabBarLabel: "Mapa",
+                tabBarIcon: ({ focused, color, size }) => (
+                    <Ionicons
+                        name={focused ? "location-outline" : "location-sharp"}
+                        color={color}
+                        size={size}
+                    />
+                )
+            }} />
+            <Screen name="Report" component={Report} options={{
+                tabBarLabel: "Reportar",
+                tabBarIcon: ({ focused, color, size }) => (
+                    <Ionicons
+                        name={focused ? "megaphone-outline" : "megaphone"}
+                        color={color}
+                        size={size}
+                    />
+                )
+            }} />
+            <Screen name="Profile" component={Profile} options={{
+                tabBarLabel: "Perfil",
+                tabBarIcon: ({ focused, color, size }) => (
+                    <Ionicons
+                        name={focused ? "person-outline" : "person-sharp"}
+                        color={color}
+                        size={size}
+                    />
+                )
+            }} />
         </Navigator>
     )
 }
