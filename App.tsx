f5185b8a37370bb5489ebe3f77a8eee1221e543f
João Darwin <joaodarwin.ip22@gmail.com>
2024-09-04 21:03:37 -0300

    feat: Fazer login automático quando token estiver no async storage

--- App.tsx
+++ App.tsx
@@ -1,12 +1,41 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { NavigationContainer } from '@react-navigation/native';
+import { useContext, useEffect, useState } from 'react';
 import useCustomFonts from './assets/fonts/useFonts';
-import { AuthProvider, AuthContext } from './src/contexts/AuthContext';
+import api from './src/api/api';
+import { AuthContext, AuthProvider } from './src/contexts/AuthContext';
 import BottomTabNavigation from './src/routes/private/BottomTabNavigation';
 import StackNavigation from './src/routes/public/StackNavigation';
-import { useContext } from 'react';
 
 function AppContent() {
-  const { isAuth } = useContext(AuthContext);
+  const { isAuth, setIsAuth } = useContext(AuthContext);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function getToken() {
+      try {
+        const token = await AsyncStorage.getItem("token");
+
+        if (token) {
+          api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+          setIsAuth(true);
+        } else {
+          setIsAuth(false);
+        }
+      } catch (error) {
+        console.error("Failed to get token:", error);
+        setIsAuth(false);
+      }
+
+      setIsLoading(false);
+    }
+
+    getToken();
+  }, [])
+
+  if (isLoading) {
+    return
+  }
 
   return (
     <NavigationContainer>
