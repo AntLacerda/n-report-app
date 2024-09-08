d0a412376c7241a646071760e0c1de7f790181e8
João Darwin <joaodarwin.ip22@gmail.com>
2024-09-08 00:37:20 -0300

    feat: Implementar lógica com funções para logout
    Issue: #12

--- src/screens/Profile/Profile.tsx
+++ src/screens/Profile/Profile.tsx
@@ -1,4 +1,4 @@
-import { Text, TouchableOpacity } from "react-native";
+import { Alert, Text, TouchableOpacity } from "react-native";
 import ContainerScreen from "../../components/ContainerScreen";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { ParamListBase } from "@react-navigation/native";
@@ -15,6 +15,21 @@ const Profile = ({ navigation }: Props) => {
     const { setIsAuth } = useContext(AuthContext);
 
     const handleLogout = async () => {
+        Alert.alert("Logout?", "Tem certeza que deseja fazer logout?", [
+            {
+                text: "Não",
+                style: "cancel"
+            },
+            {
+                isPreferred: true,
+                text: "Sim",
+                onPress: doLogout,
+                style: "default"
+            }
+        ])
+    }
+
+    const doLogout = async () => {
         try {
             await AsyncStorage.removeItem('token');
         } catch(error) {
