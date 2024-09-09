743654527492d0190603762f6dc8fe15be7ba411
João Darwin <joaodarwin.ip22@gmail.com>
2024-09-09 01:21:37 -0300

    feat: Adicionar validação para quando tudo deve ter a cor vermelha
    Issue: #12

--- src/components/ProfileOption.tsx
+++ src/components/ProfileOption.tsx
@@ -4,22 +4,26 @@ import { StyleSheet, Text, TouchableNativeFeedback, View } from "react-native"
 interface Props {
     iconName: any,
     title: string,
+    hasBorderBottom?: boolean,
+    isAllRedColor?: boolean
     onPress: () => void
 }
 
-const ProfileOption = ({ iconName, title, onPress }: Props): React.JSX.Element => {
+const ProfileOption = ({ iconName, title, hasBorderBottom, isAllRedColor, onPress }: Props): React.JSX.Element => {
+    const color = isAllRedColor ? '#F24E1E' : '#FFFFFF'
+
     return (
         <TouchableNativeFeedback onPress={onPress}>
-            <View style={styles.ProfileOption}>
+            <View style={[styles.ProfileOption, hasBorderBottom ? {borderBottomColor: color, borderBottomWidth: 0.2} : {}]}>
                 <Ionicons
                     name={iconName}
-                    color={'#3BC9DB'}
+                    color={isAllRedColor ? color : '#3BC9DB'}
                     size={24}
                 />
-                <Text style={{fontSize: 16, color: '#FFFFFF'}}>{title}</Text>
+                <Text style={{fontSize: 16, color: color}}>{title}</Text>
                 <Ionicons
                     name={"arrow-forward-sharp"}
-                    color={'#FFFFFF'}
+                    color={color}
                     size={24}
                 />
             </View>
@@ -34,7 +38,6 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'space-between',
         paddingHorizontal: 16,
-        borderBottomWidth: 1,
         display: 'flex',
         flexDirection: 'row'
     }
