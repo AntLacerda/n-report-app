3098cc3397ebaf68fc3986864268707b97cb8595
AntLacerda <lacerdaneto99@hotmail.com>
2024-09-10 08:04:47 -0300

    Feat: Desenvolvendo mapa e marcações
    Issue: 11

--- src/screens/Report/index.tsx
+++ src/screens/Report/index.tsx
@@ -1,12 +1,66 @@
-import { Text } from "react-native";
-import ContainerScreen from "../../components/ContainerScreen";
+import { Text, View, StyleSheet } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+import { 
+    requestForegroundPermissionsAsync,
+    getCurrentPositionAsync,
+    LocationObject
+} from "expo-location";
+import { useState, useEffect } from "react";
 
 const Report = () => {
+    const [location, setLocation] = useState<LocationObject | null>(null);
+
+    const requestLocationPermissions = async () => {
+        const { granted } = await requestForegroundPermissionsAsync();
+
+        if(granted) {
+            const currentPosition = await getCurrentPositionAsync();
+            setLocation(currentPosition);
+        }
+    }
+
+    useEffect(() => {
+        requestLocationPermissions();
+    }, []);
+
     return (
-        <ContainerScreen>
+        <View>
+            <View style={style.containerMap}>
+                {location ? (
+                    <MapView 
+                        style={style.map}
+                        initialRegion={{
+                            latitude: location.coords.latitude,
+                            longitude: location.coords.longitude,
+                            latitudeDelta: 0.005,
+                            longitudeDelta: 0.005,
+                        }}
+                    >
+                        <Marker
+                            coordinate={{
+                                latitude: location.coords.latitude,
+                                longitude: location.coords.longitude,
+                            }}
+                        />
+                    </MapView>
+                ) : (
+                    <Text>Carregando mapa...</Text>
+                )}
+            </View>
             <Text>Home</Text>
-        </ContainerScreen>
+        </View>
     )
 }
+
+const style = StyleSheet.create({
+    containerMap: {
+        width: '100%',
+        height: 300,
+    },
+    map: {
+        width: '100%',
+        height: '100%',
+    }
+});
 
 export default Report;
