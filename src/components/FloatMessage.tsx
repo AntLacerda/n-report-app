7ec3eaf66bc10eaea2cff39a157be41437b8d2db
João Darwin <joaodarwin.ip22@gmail.com>
2024-09-12 23:29:54 -0300

    feat: Adicionar shadow na messagem flutuante na tela de mapa
    Issue: #8

--- src/components/FloatMessage.tsx
+++ src/components/FloatMessage.tsx
@@ -23,7 +23,15 @@ const styles = StyleSheet.create({
         alignItems: "center",
         justifyContent: "center",
         padding: 12,
-        paddingHorizontal: 23
+        paddingHorizontal: 23,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 5,
+        },
+        shadowOpacity: 0.34,
+        shadowRadius: 6.27,
+        elevation: 10,
     },
     Text: {
         fontWeight: "400",
