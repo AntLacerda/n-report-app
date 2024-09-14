ff95e0a69f695658f0e4cc5fd96e1fd98b8d71ae
João Darwin <joaodarwin.ip22@gmail.com>
2024-09-14 14:43:41 -0300

    feat: Criar tela de resumo da ocorrencia
    Issue: #10

---
+++ src/screens/OcurrenceSummary/index.tsx
@@ -0,0 +1,12 @@
+import { Text } from "react-native"
+import ContainerScreen from "../../components/ContainerScreen"
+
+const OcurrenceSummary = (): React.JSX.Element => {
+    return (
+        <ContainerScreen>
+            <Text>OcurrenceSummary</Text>
+        </ContainerScreen>
+    )
+}
+
+export default OcurrenceSummary;
