bbcff1de4a8b57f927fc1b2b50976e1f574f5875
João Darwin <joaodarwin.ip22@gmail.com>
2024-09-18 12:26:39 -0300

    feat: Adicionar flatList com chamada a api com todas as ocorrencias
    Issue: #13

--- src/screens/Profile/ReportList.tsx
+++ src/screens/Profile/ReportList.tsx
@@ -1,17 +1,48 @@
-import { Text } from "react-native"
-import ContainerScreen from "../../components/ContainerScreen"
-import { useIsFocused } from "@react-navigation/native"
+import { useIsFocused } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
+import { FlatList, StyleSheet, Text } from "react-native";
+import ContainerScreen from "../../components/ContainerScreen";
+import { useEffect, useState } from "react";
+import Ocurrence from "../../interfaces/Ocurrence";
+import api from "../../api/api";
 
 const ReportList = () => {
+    const [ocurrences, setOcurrences] = useState<Ocurrence[]>([]);
     const isFocused = useIsFocused();
+
+    useEffect(() => {
+        (async () => {
+            const ocurrencesApi: Ocurrence[] = (await api.get("api/v1/ocurrences")).data;
+            setOcurrences(ocurrencesApi);
+        })();
+    }, []);
 
     return (
         <ContainerScreen>
             {isFocused && <StatusBar backgroundColor="#3BC9DB" style="light" animated={true} />}
-            <Text>ReportList</Text>
+            <FlatList
+                    contentContainerStyle={styles.Content}
+                    data={ocurrences}
+                    renderItem={({ item, index }) => (
+                        <Text style={styles.Text}>{item.title}</Text>
+                    )}
+                    ListEmptyComponent={() => (
+                        <Text style={styles.Text}>A ocorrência não possui nenhuma imagem!</Text>
+                    )}
+            />
         </ContainerScreen>
     )
 }
+
+const styles = StyleSheet.create({
+    Content: {
+        alignItems: 'center'
+    },
+    Text: {
+        fontSize: 15.89,
+        fontWeight: "400",
+        color: "#FFFFFF"
+    }
+});
 
 export default ReportList;
